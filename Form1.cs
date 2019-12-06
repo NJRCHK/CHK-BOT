6fd530e13644f1646133525981a6b3258dc6dc25
Nathaniel Roberts <nathanieljroberts11@gmail.com>
2019-12-05 17:46:11 -0800

    Runelite now opens when button is pressed

--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading; 
+
 
 namespace WindowsFormsApp1TESTING
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hwc, IntPtr hwp);
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //To be changed
+            Process p = Process.Start("C:\\Users\\Nathaniel\\AppData\\Local\\RuneLite\\RuneLite.exe");
+            Thread.Sleep(500);
+            p.WaitForInputIdle();
+            SetParent(p.MainWindowHandle, this.Handle);
+        }
         public Form1()
         {
             InitializeComponent();
