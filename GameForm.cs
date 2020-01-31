3a07ee39e888a0503f4b60d85895df6fa83afd76
Nathaniel Roberts <nathanieljroberts11@gmail.com>
2020-01-30 23:13:45 -0800

    added mouseclick function

--- GameForm.cs
+++ GameForm.cs
@@ -8,6 +8,14 @@ namespace CHK
 {
     public partial class GameForm : Form
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
         private const int WindowDockingDelay = 15000;
 
         [DllImport("user32.dll")]
@@ -17,9 +25,9 @@ namespace CHK
         {
             InitializeComponent();
             //Set window to fullscreen
-            FormBorderStyle = FormBorderStyle.None;
-            WindowState = FormWindowState.Maximized;
-            TopMost = true;
+            //FormBorderStyle = FormBorderStyle.None;
+            //WindowState = FormWindowState.Maximized;
+            //TopMost = true;
             //Open RuneLite inside of form
             var process = new Process
             {
@@ -34,6 +42,19 @@ namespace CHK
         {
             if (e.Control == true)
                 Application.Exit();
+            if(Keys.K == e.KeyCode)
+            {
+                DoMouseClick();
+            }
+
+        }
+
+        public void DoMouseClick()
+        {
+            //Call the imported function with the cursor's current position
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
         }
     }
 }
